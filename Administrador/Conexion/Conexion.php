11706fdde181c02513f74bfdaa9aa70da75bb463
vamendoza <vamendoza19@misena.edu.co>
2021-11-29 15:58:01 -0500

    Base de datos Hostinger

--- Administrador/Conexion/Conexion.php
+++ Administrador/Conexion/Conexion.php
@@ -1,14 +1,13 @@
 <?php  
-	$contrasena = '';
-	$usuario = 'root';
-	$nombrebd= 'databasefruvermcp';
+	$contrasena = 'FruverMCP123';
+	$usuario = 'u991668360_fruvermcp';
+	// $port= '3306';
+	$nombrebd= 'u991668360_fruvermcp';
+	
 
 	try {
-		$bd = new PDO(
-			'mysql:host=localhost;
-			dbname='.$nombrebd,
-			$usuario,
-			$contrasena,
+		$bd = new PDO('mysql:host=sql437.main-hosting.eu;port=3306;dbname='.$nombrebd,$usuario,$contrasena,
+			
 			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 		);
 	} catch (Exception $e) {
